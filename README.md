a698d938f46bbfd09316efa44e3fbf53016cb658
Scott Davey <sdavey@datalink.com.au>
2022-07-18 16:06:37 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,60 @@
+# testproject-wait-for-spinner
+
+Waits patiently for a spinner to disappear from the UI.
+
+It has these defaults:
+
+- xpath of element to find: `.spinner`
+- time between each check: `1 second`
+- maximum number of checks: `120`
+- number of sequential checks to make to declare the spinner gone: `2`
+
+In theory, it should have configurable settings within TestProject, but they're not 
+currently exposed because we used a deprecated annotation and the docs aren't clear 
+on how the new way works. So if you need to change these then you need to edit the code.
+
+## Developer token
+
+I downloaded a developer token as instructed, but instructions beyond that point were
+unclear, so it's not part of this code. I don't think it's needed.
+
+## Build
+
+You need to first download the TestProject SDK, which is available from their website.
+
+Download it and place it into: `/sdk/io.testproject.sdk.java.jar`.
+
+I haven't included it here as it's not mine to license.
+
+This has been built against version 0.65.0 of the TestProject SDK using Gradle 7 from within IntelliJ IDEA.
+If you update the SDK, you must manually update `src/main/testproject-sdk.properties`
+
+The `build.gradle` file tells Gradle how to build the plugin. You'll find the `.jar` file in 
+`build/libs/WaitForSpinner.jar` once it's built.
+
+## Developing
+
+Some references to get started:
+
+- https://docs.testproject.io/testproject-addons/develop-an-addon
+- https://github.com/testproject-io/addons/
+- https://intercom.help/testprojectio/en/articles/4570786-how-to-create-jar-file-from-java-project-with-testproject-sdk-and-upload-it-as-a-coded-test
+
+The code is `WaitForSpinner.java`. 
+
+It's pretty self-explanatory — here's some pseudocode.
+
+```
+do {
+   check for spinner
+   wait
+} while (spinning && not timed out)
+```
+
+## Tests
+
+Sorry, none. I'm not a Java developer so this is the best you're gonna get.
+
+## License, warranty & Support
+
+Licensed under MIT. No warranty or support. Good luck!
---
+++ src/main/java/au/com/datalink/testproject/WaitForSpinnerAction.java
@@ -0,0 +1,72 @@
+package au.com.datalink.testproject;
+
+import io.testproject.java.annotations.v2.Action;
+import io.testproject.java.annotations.v2.ActionParameter;
+import io.testproject.java.sdk.v2.addons.WebAction;
+import io.testproject.java.sdk.v2.addons.helpers.WebAddonHelper;
+import io.testproject.java.sdk.v2.drivers.WebDriver;
+import io.testproject.java.sdk.v2.enums.ExecutionResult;
+import io.testproject.java.sdk.v2.exceptions.FailureException;
+import io.testproject.java.sdk.v2.reporters.ActionReporter;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.NoSuchElementException;
+
+@Action(name = "Wait for Spinner")
+public class WaitForSpinnerAction implements WebAction {
+
+    @ActionParameter
+    public String xpath = ".spinner";
+
+    @ActionParameter
+    public int sleep = 1000; // ms
+
+    @ActionParameter
+    public int maxTries = 120; // 120 x 1 second tries by default
+
+    @ActionParameter
+    public int triesNeededToPass = 2;
+
+    public ExecutionResult execute(WebAddonHelper helper) throws FailureException {
+        // Get Driver
+        WebDriver driver = helper.getDriver();
+        ActionReporter report = helper.getReporter();
+
+        // Get the xpath element
+        By by = By.xpath(xpath);
+
+        int triesNotFound = 0; // number of times it has not found the element
+        int triesOverall = 0; // number of tries overall
+
+        do {
+            triesOverall++;
+            try {
+                // Try to locate the element
+                WebElement webElement = driver.findElement(by);
+                // reset the not found counter if it found the element
+                triesNotFound = 0;
+            } catch (NoSuchElementException e) {
+                // not found, increment.
+                triesNotFound++;
+            }
+
+            // Now sleep
+            try {
+                Thread.sleep(this.sleep);
+            } catch (InterruptedException ex) {
+                Thread.currentThread().interrupt();
+            }
+
+            // exit the loop after max tries, or after the element was confirmed to not exist
+        } while (triesOverall < this.maxTries && triesNotFound < this.triesNeededToPass);
+
+        if (triesNotFound == this.triesNeededToPass) {
+            report.result(String.format("Element not present after %d tries", triesOverall));
+            return ExecutionResult.PASSED;
+        } else {
+            report.result(String.format("Element still present after %d tries", triesOverall));
+            return ExecutionResult.FAILED;
+        }
+    }
+}
